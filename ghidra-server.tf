6392e156bfbaae6ac30025c9e9b98ce43acc88b1
Pheenoh <pheenoh@gmail.com>
2022-10-02 22:16:58 -0600

    rm new var

--- ghidra-server.tf
+++ ghidra-server.tf
@@ -6,5 +6,5 @@ module "ghidra" {
   aws_create_dns_record = true
   aws_dns_zone_name     = "tpgz.io"
   aws_dns_record_name   = "newghidra"
-  ghidra_uri_override   = "https://pheenoh-tp-community.s3.us-east-2.amazonaws.com/ghidra_10.2_DEV_20221002_mac_arm_64.zip"
+  # ghidra_uri_override   = "https://pheenoh-tp-community.s3.us-east-2.amazonaws.com/ghidra_10.2_DEV_20221002_mac_arm_64.zip"
 }
