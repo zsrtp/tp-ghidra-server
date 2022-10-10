7886e529d5967054b5a9ccf056bdd8d3c0c5623a
Pheenoh <pheenoh@gmail.com>
2022-10-10 11:47:05 -0600

    use ghidra 10.2, update dns name

--- ghidra-server.tf
+++ ghidra-server.tf
@@ -5,7 +5,8 @@ module "ghidra" {
   aws_subnet_id              = "subnet-0936e10d4096f910b"
   aws_create_dns_record      = true
   aws_dns_zone_name          = "tpgz.io"
-  aws_dns_record_name        = "newghidra"
+  aws_dns_record_name        = "ghidra"
+  ghidra_version             = "10.2"
   ghidra_uri_override        = "https://pheenoh-tp-community.s3.us-east-2.amazonaws.com/ghidra_10.2_DEV_20221002_linux_x86_64.zip"
   ghidra_zip_file_name       = "ghidra_10.2_DEV_20221002_linux_x86_64.zip"
   ghidra_extract_folder_name = "ghidra_10.2_DEV"
